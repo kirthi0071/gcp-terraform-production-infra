a739633005e7246c6cf1f27845f5aa71464489ff
kirthi0071 <kirthikumar077@gmail.com>
2026-08-16 14:20:34 +0530

    new

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,7 +1,9 @@
 variable "project_id" { type = string }
 variable "name_prefix" { type = string }
 variable "region" { type = string }
-variable "zone" { type = string }
+variable "zones" {
+  type = list(string)
+}
 variable "machine_type" { type = string }
 variable "image" { type = string }
 variable "subnet_self_link" { type = string }
